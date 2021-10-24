5700800b5d75537166643836f311325399f8f18e
shahroz <shehrozakbar@gmail.com>
2021-10-24 23:23:41 +0500

    init

---
+++ src/containers/App/App.jsx
@@ -0,0 +1,17 @@
+import "../../App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="card">
+        <div className="card-body">
+          <button className="btn btn-primary"> Welcome To ITOT </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
